c3c9b05c852697486dfe6ffe11a51e657ba63eb6
Matt Williamson <mattwilliamson@boisestate.edu>
2022-02-11 11:50:15 -0700

    Create 04_fun_K_lcp.R

---
+++ Scripts/R/04_fun_K_lcp.R
@@ -0,0 +1,72 @@
+library(raster)
+library(gdistance)
+library(yenpathy)
+library(rgdal)
+library(sf)
+library(magrittr)
+
+# Function for low cost paths ---------------------------------------------
+gen_top_paths <- function(tr, resist,  numpath, bufdist, orig, goal){
+  originCells <- raster::cellFromXY(tr, orig)
+  goalCells <- raster::cellFromXY(tr, goal)
+  indexOrigin <- originCells 
+  indexGoal <- goalCells 
+  result.list <- vector("list",numpath)
+  update.res.list <- vector("list",numpath)
+  for(z in 1:numpath){
+    if(z == 1){
+      y <- transitionMatrix(tr)
+      if(isSymmetric(y)) {
+        mode <- "undirected"
+      }else{
+        mode <- "directed"
+      }
+      adjacencyGraph <- igraph::graph.adjacency(y, mode=mode, weighted=TRUE)
+      E(adjacencyGraph)$weight <- 1/E(adjacencyGraph)$weight
+      shortestPaths <- get.shortest.paths(adjacencyGraph,
+                                          indexOrigin, indexGoal)$vpath
+      result <- tr
+      transitionMatrix(result) <- Matrix::Matrix(0, ncol=ncell(tr), nrow=ncell(tr))
+      for(i in 1:length(shortestPaths)){
+        sPVector <- shortestPaths[[i]]
+        adj <- cbind(sPVector[-(length(sPVector))], sPVector[-1])
+        adj <- rbind(adj,cbind(adj[,2], adj[,1]))
+        transitionMatrix(result)[adj] <- 1/length(shortestPaths) + transitionMatrix(result)[adj]}
+      result.rast <- raster(result)
+      result.buf <- raster::buffer(result.rast, width = bufdist, doEdge=TRUE)
+      result.list[[z]] <- result.buf
+      update <- tr
+      adj <- raster::adjacent(result.buf, Which(!is.na(result.buf), cells = TRUE), directions=16)
+      transitionMatrix(update)[adj] <- cellStats(raster(tr), min)
+      update.res.list[[z]] <- update
+    }else{
+      y <- transitionMatrix(update.res.list[[z-1]])
+      if(isSymmetric(y)) {
+        mode <- "undirected"
+      }else{
+        mode <- "directed"
+      }
+      adjacencyGraph <- igraph::graph.adjacency(y, mode=mode, weighted=TRUE)
+      E(adjacencyGraph)$weight <- 1/E(adjacencyGraph)$weight
+      shortestPaths <- get.shortest.paths(adjacencyGraph,
+                                          indexOrigin, indexGoal)$vpath
+      result <- tr
+      transitionMatrix(result) <- Matrix::Matrix(0, ncol=ncell(tr), nrow=ncell(tr))
+      for(i in 1:length(shortestPaths)){
+        sPVector <- shortestPaths[[i]]
+        adj <- cbind(sPVector[-(length(sPVector))], sPVector[-1])
+        adj <- rbind(adj,cbind(adj[,2], adj[,1]))
+        transitionMatrix(result)[adj] <- 1/length(shortestPaths) + transitionMatrix(result)[adj]}
+      result.rast <- raster(result)
+      result.buf <- raster::buffer(result.rast, width = bufdist, doEdge=TRUE)
+      result.list[[z]] <- result.buf
+      update <- update.res.list[[z-1]] 
+      adj <- raster::adjacent(result.buf, Which(!is.na(result.buf), cells = TRUE), directions=16)
+      transitionMatrix(update)[adj] <- cellStats(raster(tr), min)
+      update.res.list[[z]] <- update
+    }
+  }
+  all.res <- list(result.list, update.res.list)
+  return(all.res)
+}
+
