04cd5689dd195b82684da25882354da69d5da3bd
Matt Williamson <mattwilliamson@hes-mwilliamsonmac.boisestate.edu>
2022-02-11 13:28:15 -0700

    update paths

--- Scripts/R/03_fuzzy_sum_social.R
+++ Scripts/R/03_fuzzy_sum_social.R
@@ -1,9 +1,9 @@
 library(terra)
 
-house.val <- rast(here::here("data/ProcessedData/landval_crop_resamp.tif"))  #already 0-1
-wolves <- rast(here::here('data/ProcessedData/wolf_crop_resamp.tif'))   #0-1 but not scaled
-cattle <- rast(here::here('data/ProcessedData/cattle_num.tif'))  #0-1 rescale of log(cattle num)
-jurisdiction <- rast(here::here('data/ProcessedData/federal_instcap_rast.tif'))
+house.val <- rast(here::here("data/ProcessedData/rasters/landval_crop_resamp.tif"))  #already 0-1
+wolves <- rast(here::here('data/ProcessedData/rasters/wolf_crop_resamp.tif'))   #0-1 but not scaled
+cattle <- rast(here::here('data/ProcessedData/rasters/cattle_num.tif'))  #0-1 rescale of log(cattle num)
+jurisdiction <- rast(here::here('data/ProcessedData/rasters/federal_instcap_rast.tif'))
 
 wolves.rscl <- (raster::raster(wolves) - raster::cellStats(raster::raster(wolves), min))/(raster::cellStats(raster::raster(wolves), max)-raster::cellStats(raster::raster(wolves), min))
 
@@ -15,6 +15,6 @@ jurisdiction.1m <- 1 - jurisdiction
 imp.fuz.sum1 <- 1 - ((wolves.1m) * (cattle.1m) * (jurisdiction.1m))
 #imp.fuz.sum2 <- 1 - ((wolves.1m) * (cattle.1m) * (jurisdiction.1m) * (house.val.1m))
 #imp.fuz.sum[is.na(imp.fuz.sum)] <- 5 #give NAs (lots of water) high resistance vals
-writeRaster(imp.fuz.sum1, here::here('data/ProcessedData/imp_fuz_sum_nohouse.tif'), overwrite=TRUE)
+writeRaster(imp.fuz.sum1, here::here('data/ProcessedData/rasters/imp_fuz_sum_nohouse.tif'), overwrite=TRUE)
 #writeRaster(imp.fuz.sum2, here::here('Data/ProcessedData/imp_fuz_sum.tif'), overwrite=TRUE)
 
