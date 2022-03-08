a5cd4c05cd8a6333faee8dda2cd8829e0311480f
Matt Williamson <mattwilliamson@hes-mwilliamsonmac.boisestate.edu>
2022-03-08 12:12:06 -0700

    split plot scripts

---
+++ Scripts/R/06_plot_fig1_lcc_cs.R
@@ -0,0 +1,158 @@
+library(raster)
+library(gdistance)
+library(rgdal)
+library(sf)
+library(magrittr)
+library(ggplot2)
+library(purrr)
+library(dplyr)
+library(rasterVis)
+library(patchwork)
+library(classInt)
+library(cowplot)
+library(ggsci)
+library(stringr)
+library(egg)
+# load Circuitscape results -----------------------------------------------
+
+biophys.cs <- raster(here::here('data/ProcessedData/biophys/biophys_cum_curmap.asc'))
+
+
+# Load centroids ----------------------------------------------------------
+origins <- st_read(here::here("Data/ProcessedData/shapefiles/studyPAcentroids.shp")) %>% 
+  dplyr::filter(., Unit_Nm == "Weminuche Wilderness") %>% 
+  as(. , "Spatial")
+goals <- st_read(here::here("Data/ProcessedData/shapefiles/studyPAcentroids.shp")) %>% 
+  dplyr::filter(., Unit_Nm == "Yellowstone National Park") %>% 
+  as(. , "Spatial")
+origin.proj <- spTransform(origins, crs(biophys.cs))
+goals.proj <- spTransform(goals, crs(biophys.cs))
+
+
+# Load k cost paths -------------------------------------------------------
+
+biophys <- readRDS(here::here('Data/ProcessedData/TransitionLayers/biophystop5.rds'))
+biophys.lst <- biophys[[1]]
+
+
+# Custom map theme --------------------------------------------------------
+
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      #text = element_text(family = "Ubuntu Regular", color = "#22211d"),
+      axis.line = element_line(color = "black", size=0.02),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "white", size = 0.002),
+      panel.grid.minor = element_line(color = "white", size = 0.002),
+      plot.background = element_rect(fill = "white", color = NA), 
+      panel.background = element_rect(fill = "white", color = NA), 
+      legend.background = element_rect(fill = "white", color = NA),
+      #panel.border = element_rect(fill=NA, color = "black"),
+      ...
+    )
+}
+# Load Elev data and calc hillshade ---------------------------------------
+elev <- getData('alt', country = 'USA')
+elev.proj <- projectRaster(elev[[1]], biophys.cs)
+
+
+elev.crop <- crop(elev.proj, biophys.cs)
+elev.mod <- elev.crop *10
+slope <- terrain(elev.mod, opt='slope')
+aspect <- terrain(elev.mod, opt='aspect')
+hill = hillShade(slope, aspect, 40, 270)
+hill2 <- aggregate(hill , fact = 5 , method = "bilinear" )
+hills3 <- focal(hill2, w=matrix(1/9, nc=3, nr=3), mean)
+
+
+# Get vectors for maps ----------------------------------------------------
+
+PAs <- st_read(here::here("Data/ProcessedData/shapefiles/studyPAs.shp")) %>% 
+  dplyr::filter(. , Unit_Nm == "Yellowstone National Park" | Unit_Nm == "Weminuche Wilderness") %>% 
+  st_transform(. , crs = crs(biophys.cs))
+
+sts <- tigris::states() %>% 
+  dplyr::filter(., STUSPS %in% c("ID", "MT", "UT", "WY", "CO", "AZ", "NM")) %>% 
+  st_transform(., crs(biophys.cs)) %>% 
+  as(., "Spatial")
+
+conus <-  tigris::states() %>%
+  dplyr::filter(,, !STUSPS %in% c("AK","AS", "HI", "GU", "VI", "DC", "PR", "MP")) %>% 
+  st_transform(., crs(biophys.cs))
+
+
+sts.crop <- crop(sts, biophys.cs)
+
+pa.cents <- rbind(as(origin.proj,"sf"), as(goals.proj, "sf"))
+pa.cents$lab <- c("Weminche \n Wilderness Area", "Yellowstone \n National Park")
+# Plot Circuitscape results -----------------------------------------------
+
+
+b_df <- biophys.cs %>%
+  rasterToPoints %>%
+  as.data.frame() %>%
+  `colnames<-`(c("x", "y", "biophys"))
+
+biophys.lcps <- lapply(biophys.lst, function (x) rasterToPolygons(x=x, n=8, dissolve = TRUE))
+
+biophys.lcp.sf <- list(biophys.lcps, makeUniqueIDs = T) %>% 
+  flatten() %>% 
+  do.call(rbind, .) %>% 
+  as(., "sf") %>% 
+  dplyr::mutate(., rank = row_number())
+
+
+
+p.cs <- RStoolbox::ggR(hills3) +
+  geom_raster(
+    data = b_df,
+    aes(
+      x=x,
+      y=y,
+      fill=biophys
+    ),
+    alpha=0.8,
+    interpolate = TRUE
+  ) +
+  scale_fill_viridis_c(option="B", 
+                       limits=c(-0.000000001,0.005), 
+                       breaks = c(0,0.005),
+                       labels = c("Low","High")) +
+  geom_sf(data=biophys.lcp.sf, aes(color=as.character(rank)), fill=NA, lwd=1.15) +
+  scale_color_locuszoom() +
+  geom_sf(data = PAs, fill = "forestgreen") +
+  geom_sf(data = as(sts.crop, "sf"), fill = NA, color="white")+
+  ggrepel::geom_text_repel(data = pa.cents, size = 1.75, aes(x = st_coordinates(pa.cents)[,1], y = st_coordinates(pa.cents)[,2], label=lab), nudge_x = -40000 , nudge_y = c(80000,90000), fontface="bold", color = "white")+
+  guides(fill = guide_colorbar(nbin = 8,  title.position = "left", 
+                               label.position="right", title = "Current flow", barheight = 2.25, barwidth = 1), 
+         color = guide_legend(title.position = "left", 
+                              label.position="right", title = "Cost rank", keywidth = 1, keyheight = 1))+
+  theme_map() + theme(legend.position = "left", 
+                      legend.box = "vertical", 
+                      legend.title = element_text(angle = 90, size = 3.75),
+                      legend.text = element_text(size = 3.5),
+                      plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), units = "pt"),
+                      legend.box.margin = unit(c(0, -0.1, 0.1, 0.1), units = "pt"))
+
+inset <- ggplot()+
+  geom_sf(data=conus, fill="white") +
+  geom_sf(data =st_as_sfc(st_bbox(biophys.cs)), fill=NA, color="red") +
+  theme_map() +
+  theme(panel.background = element_rect(fill = "gray", color = "black"),
+        plot.background = element_rect(fill = NA, color = NA))  
+
+
+p.combined <- ggdraw(p.cs) +
+  draw_plot(inset, x = 0.33, y = 0,  
+            width = 0.3, height = 0.15)
+
+
+
+cowplot::save_plot(here::here("plots/fig1.png"), plot =p.combined, base_height = 4)
+
---
+++ Scripts/R/07_plot_curdens_costratio.R
@@ -0,0 +1,170 @@
+library(raster)
+library(gdistance)
+library(rgdal)
+library(sf)
+library(magrittr)
+library(ggplot2)
+library(rasterVis)
+library(patchwork)
+library(classInt)
+library(cowplot)
+library(ggsci)
+library(stringr)
+library(egg)
+# Load transition layers --------------------------------------------------
+social.tr1 <- readRDS(here::here('data/ProcessedData/TransitionLayers/socialtrans1.rds'))
+biophys.tr <- readRDS(here::here('data/ProcessedData/TransitionLayers/biophystrans.rds'))
+jurisdiction.tr <- readRDS(here::here('data/ProcessedData/TransitionLayers/socialtrans_jurisdiction.rds'))
+cattle.tr <- readRDS(here::here('data/ProcessedData/TransitionLayers/socialtrans_cattle.rds'))
+
+# Load Resistance surfaces ------------------------------------------------
+biophys.resist <- raster(here::here("data/ProcessedData/ResistanceSurfaces/biophys_resist.tif"))
+implementation.resist1 <- raster(here::here("data/ProcessedData/ResistanceSurfaces/implement_resist.tif"))
+implementation.resist2 <-  raster(here::here('data/ProcessedData/ResistanceSurfaces/implement_resist_jurisdiction.tif'))
+implementation.resist3 <-  raster(here::here('data/ProcessedData/ResistanceSurfaces/implement_resist_cattle.tif'))
+# Load centroids ----------------------------------------------------------
+origins <- st_read(here::here("Data/ProcessedData/shapefiles/studyPAcentroids.shp")) %>% 
+  dplyr::filter(., Unit_Nm == "Weminuche Wilderness") %>% 
+  as(. , "Spatial")
+goals <- st_read(here::here("Data/ProcessedData/shapefiles/studyPAcentroids.shp")) %>% 
+  dplyr::filter(., Unit_Nm == "Yellowstone National Park") %>% 
+  as(. , "Spatial")
+origin.proj <- spTransform(origins, crs(biophys.resist))
+goals.proj <- spTransform(goals, crs(biophys.resist))
+
+
+# Load k cost paths -------------------------------------------------------
+
+social1 <- readRDS(here::here('Data/ProcessedData/TransitionLayers/socialtop5.rds'))
+biophys <- readRDS(here::here('Data/ProcessedData/TransitionLayers/biophystop5.rds'))
+
+
+# Create Euclidean distance-----------------------------------------------------
+
+euclidean.resist <- biophys.resist
+values(euclidean.resist) <- 1
+euclidean.tr <- transition(1/euclidean.resist, transitionFunction = mean, 16)
+euclidean.tr <- geoCorrection(euclidean.tr, "c")
+eucdist <- accCost(euclidean.tr, origin.proj)
+
+#extract values for last 50km
+euc.dist <- st_distance(as(origin.proj, "sf"), as(goals.proj, "sf"))
+units(euc.dist) <- NULL
+threshold <- as.vector(euc.dist - 10000)
+# Extract cost and current values -----------------------------------------
+
+
+social1.lst <- social1[[1]]
+#social2.lst <- social2[[1]]
+biophys.lst <- biophys[[1]]
+#all.lst <- c(social1.lst, social2.lst, biophys.lst)
+all.lst <- c(social1.lst, biophys.lst)
+soc.costdist1 <- accCost(social.tr1, origin.proj)
+jurisdiction.costdist <- accCost(jurisdiction.tr, origin.proj)
+cattle.costdist <- accCost(cattle.tr, origin.proj)
+bio.costdist <- accCost(biophys.tr, origin.proj)
+#dist.stack <- stack(eucdist,bio.costdist, soc.costdist1, soc.costdist2)
+dist.stack <- stack(eucdist,bio.costdist, soc.costdist1, jurisdiction.costdist, cattle.costdist, biophys.cs)
+
+distance.extract <- lapply(1:length(all.lst), function(x) raster::extract(dist.stack, rasterToPolygons(all.lst[[x]], dissolve = TRUE)))
+#names(distance.extract) <- c("s1_1", "s1_2", "s1_3", "s1_4", "s1_5","s2_1", "s2_2", "s2_3", "s2_4", "s2_5","b1", "b2", "b3", "b4", "b5")
+names(distance.extract) <- c("s1", "s2", "s3", "s4", "s5","b1", "b2", "b3", "b4", "b5")
+
+dist.df.list <- lapply(1:length(distance.extract), function(x) as.data.frame(distance.extract[[x]]))
+names(dist.df.list) <- names(distance.extract)
+
+dist.df <- dplyr::bind_rows(dist.df.list, .id="column_label")
+#colnames(dist.df) <- c("LCPID","eucdist","bio.costdist", "soc.costdist1", "soc.costdist2")
+colnames(dist.df) <- c("LCPID","eucdist","bio.costdist", "soc.costdist1", "jurisdiction.costdist","cattle.costdist", "biophys.cur")
+
+dist.cln <- do.call(data.frame,lapply(dist.df, function(x) replace(x, is.infinite(x),NA)))
+
+# Estimate current density ------------------------------------------------
+
+current.dens <- dist.cln %>% 
+  dplyr::group_by(., LCPID) %>%
+  dplyr::filter(., startsWith(LCPID, "b")) %>% 
+  dplyr::summarise(., sum.current = round(sum(biophys.cur, na.rm = TRUE), 2),
+                   max.dist = round(max(eucdist, na.rm = TRUE)/1000, 2))  %>%
+  dplyr::mutate(., ID = str_remove_all( LCPID, fixed("b"))) %>% 
+  dplyr::select(., c(ID, max.dist, sum.current)) %>% 
+  flextable::flextable() %>% 
+  flextable::set_header_labels(., ID = "Cost Rank", max.dist = "Distance (km)", sum.current = "Total Current") 
+
+flextable::save_as_docx(current.dens, path = here::here("plots/table1.docx"))
+
+# Estimate cost.ratio -----------------------------------------------------
+library(tidyr)
+library(dplyr)
+dist.90 <- dist.cln %>% 
+  dplyr::group_by(LCPID) %>% 
+  dplyr::filter(., eucdist > threshold)
+#dist.90[,c(2:5)] <- apply(dist.90[,c(2:5)], 2, log10)
+#dist.90[,c(2:4)] <- apply(dist.90[,c(2:4)], 2, log10)
+
+max.bphys.lcp <- dist.90 %>% 
+  dplyr::group_by(., LCPID) %>%
+  dplyr::summarise(., max_biophys = max(bio.costdist, na.rm=TRUE))
+max.difs <- max.bphys.lcp
+max.difs[-1] <- lapply(max.bphys.lcp[-1], function(x) x - min(x, na.rm=TRUE))
+colnames(max.difs)[2] <- "delta_bio.costdist"
+
+dist.group <- dist.90 %>% 
+  dplyr::left_join(., max.difs) %>%
+  dplyr::mutate(., abase = soc.costdist1/bio.costdist,
+                ajuris = jurisdiction.costdist/bio.costdist,
+                bcattle = cattle.costdist/bio.costdist) %>%
+  dplyr::filter(.,  startsWith(LCPID, "b")) %>% 
+  select(., c(1,8:11)) %>% 
+  pivot_longer(., !c(LCPID, delta_bio.costdist)) %>% 
+  dplyr::mutate(., ID = str_remove_all( LCPID, fixed("b")))
+
+dist.sum <- dist.group %>% 
+  group_by(., ID, name) %>% 
+  summarise(mn = mean(value),
+            std = sd(value),
+            costdist = log(delta_bio.costdist, 10))
+
+
+p1 <- ggplot(data = filter(dist.sum, name == "abase"), mapping = aes(x = ID, 
+                                                                     y = mn, 
+                                                                     color = costdist)) +
+  geom_pointrange(mapping = aes(ymin = mn - std,
+                                ymax = mn + std)) +
+  scale_color_viridis_c(direction = -1, begin= 0, end =0.8) + 
+  ylim(110, 150) +
+  labs(x= "Cost Rank", y= "Cost ratio") + 
+  guides(color = guide_colorbar(title = "\u0394 log(biophysical \ncost)")) +
+  theme_bw()
+
+p2 <- ggplot(data = filter(dist.sum, name == "ajuris"), mapping = aes(x = ID, 
+                                                                      y = mn, 
+                                                                      color = costdist)) +
+  geom_pointrange(mapping = aes(ymin = mn - std,
+                                ymax = mn + std)) +
+  scale_color_viridis_c(direction = -1, begin= 0, end =0.8) + 
+  ylim(110, 150) +
+  labs(x= "Cost Rank", y= "Cost ratio") + 
+  guides(color = guide_colorbar(title = "\u0394 log(biophysical \ncost)")) +
+  theme_bw() +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank())
+
+p3 <- ggplot(data = filter(dist.sum, name == "bcattle"), mapping = aes(x = ID, 
+                                                                       y = mn, 
+                                                                       color = costdist)) +
+  geom_pointrange(mapping = aes(ymin = mn - std,
+                                ymax = mn + std)) +
+  scale_color_viridis_c(direction = -1, begin= 0, end =0.8) + 
+  ylim(110, 150) +
+  labs(x= "Cost Rank", y= "Cost ratio") + 
+  guides(color = guide_colorbar(title = "\u0394 log(biophysical \ncost)")) +
+  theme_bw() +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank() )
+
+combined <- p1 + p2 + p3 + plot_layout(guides = 'collect') + plot_annotation(tag_levels = "a", tag_suffix = ")")
+
+ggsave('plots/fig2.png', combined)
